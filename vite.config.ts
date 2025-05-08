bca05a75134c8ac5eadf48197e1d972d02037f74
Ranielle Canlas <raniellecanlas18@gmail.com>
2025-05-08 13:55:07 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/zerulean-tech.github.io/',
+  base: '/', // Update this to '/' for user/organization GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
